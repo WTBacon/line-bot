cf232547e67e532b9d42b603ad600e3a8d7316b4
WTBacon <wtbacon514@gmail.com>
2019-03-02 22:15:57 +0900

    涼真くんに反応するように追加

--- line-bot/main.go
+++ line-bot/main.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"regexp"
 	"time"
 
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -15,6 +16,8 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
+	channelSecret := os.Getenv("CHANNEL_SECRET")
+	channelToken := os.Getenv("CHANNEL_TOKEN")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -25,7 +28,8 @@ func main() {
 
 	router.POST("/hook", func(c *gin.Context) {
 		client := &http.Client{Timeout: time.Duration(15 * time.Second)}
-		bot, err := linebot.New("40281719617902503c0891a5bb2ebb4f", "Tmu5PIKyXgIWCb526flAvQ6HcmaSvWfB0R4aMJ6IbawLqVHbmZOFLuliL2rRk7qYOcpHgIaM/WKDZnvj1Sr0TRbS92JTOHw2d90PULl6RY23zjZxe1jyqVWV0HiPhe6smPHX9Xg3V9vhFNBvB2UOmQdB04t89/1O/w1cDnyilFU=", linebot.WithHTTPClient(client))
+		bot, err := linebot.New(channelSecret, channelToken, linebot.WithHTTPClient(client))
+
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -37,13 +41,14 @@ func main() {
 				// 変数 message のスコープを switch 文の内部に絞っている.
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					if resMessage := getResMessage(message.Text); resMessage != "" {
-						fmt.Println("resMessage : " + resMessage)
-						postMessage := linebot.NewTextMessage(resMessage)
-						fmt.Println("postMessage : " + postMessage.Text)
-						if _, err = bot.ReplyMessage(event.ReplyToken, postMessage).Do(); err != nil {
-							log.Print(err)
-
+					if convMatchingMessage(message.Text) {
+						// セミコロンで区切ると条件判定の前処理を書ける.
+						// 条件式に用いる変数のスコープを限定できる.
+						if resMessage := convResMessage(); resMessage != "" {
+							postMessage := linebot.NewTextMessage(resMessage)
+							if _, err = bot.ReplyMessage(event.ReplyToken, postMessage).Do(); err != nil {
+								log.Print(err)
+							}
 						}
 					}
 				}
@@ -55,16 +60,14 @@ func main() {
 	router.Run(":" + port)
 }
 
-func getResMessage(reqMessage string) (message string) {
-	resMessages := [3]string{"message 1", "message 2", "message 3"}
+func convMatchingMessage(targetMessage string) (matched bool) {
+	r := regexp.MustCompile(`涼真`)
+	return r.MatchString(targetMessage)
+}
 
+func convResMessage() (message string) {
+	resMessages := [3]string{"お仕事お疲れ様！\n今日も頑張って偉いね。\nたまにはゆっくり休むんだよ^^", "あんまり無理するなよ！\n心配になっちゃうからさ^^", "ちゃんとご飯食べた？\n食べないと元気でないぞ！"}
 	rand.Seed(time.Now().UnixNano())
-
-	// セミコロンで区切ると条件判定の前処理を書ける.
-	// 条件式に用いる変数のスコープを限定できる.
-	if math := rand.Intn(4); math != 3 {
-		message := resMessages[math]
-		return message + reqMessage
-	}
-	return reqMessage
+	resMessage := resMessages[rand.Intn(3)]
+	return resMessage
 }
